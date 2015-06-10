d6475f5f83432098d936eb44700e39e4d9e72b29
Jon Surrell <jon.surrell@gmail.com>
2015-06-10 19:01:04 +0200

    push/pull

--- npm_scripts/sync.sh
+++ npm_scripts/sync.sh
@@ -2,5 +2,5 @@
 set -u
 # set -x
 
-git ci -am 'push/pull' && ssh jon 'su -lc '"'"'cd corocotta_dev_site; git pull -q'"'"' corocotta'
+git ci -am 'push/pull' && git push -q && ssh jon 'su -lc '"'"'cd corocotta_dev_site; git pull -q'"'"' corocotta'
 exit 0
