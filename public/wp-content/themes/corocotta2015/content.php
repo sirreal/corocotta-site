6b24cbefb9d46376a615b6c9336b9508744abdfc
Jon Surrell <jon.surrell@gmail.com>
2015-06-10 19:23:44 +0200

    push/pull

--- public/wp-content/themes/corocotta2015/content.php
+++ public/wp-content/themes/corocotta2015/content.php
@@ -13,7 +13,7 @@
     <?php
       /* translators: %s: Name of current post */
       the_content( sprintf(
-        __( 'Continue reading %s', 'twentyfifteen' ),
+        __( 'Continue reading %s', 'corocotta' ),
         the_title( '<span class="screen-reader-text">', '</span>', false )
       ) );
 
