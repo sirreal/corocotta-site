b585f9907a5f99fa60d218c16f9cba93438e5e92
Jon Surrell <jon.surrell@gmail.com>
2015-06-10 19:24:01 +0200

    push/pull

--- public/wp-content/themes/corocotta2015/content.php
+++ public/wp-content/themes/corocotta2015/content.php
@@ -18,7 +18,7 @@
       ) );
 
       wp_link_pages( array(
-        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'corocotta' ) . '</span>',
         'after'       => '</div>',
         'link_before' => '<span>',
         'link_after'  => '</span>',
